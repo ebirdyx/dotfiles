32445dfd66b1e0a5bb44ac68efd50ccf813a5c1c
Pamela Brahollari <ebirdyxx@gmail.com>
2019-08-27 11:22:57 +0200

    Added vsCode

--- run.sh
+++ run.sh
@@ -6,3 +6,14 @@ sudo apt install -y zsh curl
 chsh --shell /usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #install oh my zsh
 
+# installing vsCode
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt update 
+sudo apt install code
+
