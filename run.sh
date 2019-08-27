d81860c1ed24ac42ed522475b4bdcaa44992a87d
Pamela Brahollari <ebirdyxx@gmail.com>
2019-08-27 10:50:07 +0200

    added zsh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 sudo apt update
 sudo apt install -y git vim chromium-browser
+
+# adding zsh
+sudo apt install -y zsh curl
+chsh --shell /usr/bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #install oh my zsh
+
